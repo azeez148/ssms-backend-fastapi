ef1cd427d34e78c5def71bb7f364b5241810dd89
azeez148 <azeez148@gmail.com>
2026-03-29 12:53:14 +0530

    fix db config

--- migrate_days.py
+++ migrate_days.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine, inspect, text
 import os
 
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-POSTGRES_SERVER = os.getenv("POSTGRES_SERVER", "localhost")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "ssms-db")
+from app.core import database
+
+POSTGRES_USER = database.POSTGRES_USER
+POSTGRES_PASSWORD = database.POSTGRES_PASSWORD
+POSTGRES_SERVER = database.POSTGRES_SERVER
+POSTGRES_PORT = database.POSTGRES_PORT
+POSTGRES_DB = database.POSTGRES_DB
 
 # Allow overriding the entire URL via environment variable
 SQLALCHEMY_DATABASE_URL = os.getenv(
